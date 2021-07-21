fb4e5b6ddc7bd3a3d2b1841ec3674ae2be11c1b7
katsu-skillup <seraphyk@gmail.com>
2021-07-21 22:39:26 +0900

    アソシエーションとバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :delivery_date_id
+    validates :price
+  end
+
+  belongs_to :user
+
 end
