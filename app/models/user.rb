dac0cf8559f7d2fb5d2e4050ba8a39cbb173bba7
katsu-skillup <seraphyk@gmail.com>
2021-07-21 19:51:41 +0900

    Validationをまとめて記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
   
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
-    validates :last_name
-    validates :first_name
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters'} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
   end
-  
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters'} do
-    validates :last_name_kana, presence: true
-    validates :first_name_kana, presence: true
-  end
- 
-  validates :birthday, presence: true
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
