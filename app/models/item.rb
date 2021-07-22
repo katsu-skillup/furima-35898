102388f31f236c693e2befd83d22129eb8baacbf
katsu-skillup <seraphyk@gmail.com>
2021-07-22 22:58:08 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,4 +15,18 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :sales_status
+    belongs_to :shipping_fee_status
+    belongs_to :prefecture
+    belongs_to :delivery_date
+
+  with_options numericality: { other_than: 1 , message: "can't be blank" } do
+    validates :category_id
+    validates :sales_status_id
+    validates :sales_status_id
+    validates :prefecture_id
+    validates :delivery_date_id
+  end
 end
