c545471055e2a0f3861abe737de38dd466fc0a80
katsu-skillup <seraphyk@gmail.com>
2021-07-23 02:42:18 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+
   with_options presence: true do
     validates :image
     validates :name
@@ -8,10 +9,10 @@ class Item < ApplicationRecord
     validates :shipping_fee_status_id
     validates :prefecture_id
     validates :delivery_date_id
-    with_options inclusion: {in: 300..999999, message: 'is out of setting range' } ,format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters'} do
-      validates :price
-    end
-  end
+    validates :price, numericality: {greater_than_or_eqal_to: 300,less_than_or_equal_to: 9999999, message: 'is out of setting range' }
+  end 
+    validates :price, numericality: {with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
+
   with_options numericality: { other_than: 1 , message: "can't be blank" } do
     validates :category_id
     validates :sales_status_id
