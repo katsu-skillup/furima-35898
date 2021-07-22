b3d33da979483587b2cf7bb4f87b5db107cec5ce
katsu-skillup <seraphyk@gmail.com>
2021-07-22 21:36:19 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,9 @@ class Item < ApplicationRecord
     validates :shipping_fee_status_id
     validates :prefecture_id
     validates :delivery_date_id
-    validates :price
+     with_options inclusion: {in: 300..999999, message: 'is out of setting range' } ,format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters'} do
+      validates :price
+     end
   end
 
   belongs_to :user
