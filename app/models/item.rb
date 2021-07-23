9d1b3d4e3bbcc01c67a269857263efa06633b52f
katsu-skillup <seraphyk@gmail.com>
2021-07-23 19:10:51 +0900

    Validationの記述ミス修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,9 +9,10 @@ class Item < ApplicationRecord
     validates :shipping_fee_status_id
     validates :prefecture_id
     validates :delivery_date_id
-    validates :price, numericality: {greater_than_or_eqal_to: 300,less_than_or_equal_to: 9999999, message: 'is out of setting range' }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range' }   
   end 
-    validates :price, numericality: {with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
+
+  validates :price, numericality: {with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
 
   with_options numericality: { other_than: 1 , message: "can't be blank" } do
     validates :category_id
