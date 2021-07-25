7c1a4095ee3d0d4b265c47a285dfb6392a846723
katsu-skillup <seraphyk@gmail.com>
2021-07-25 13:27:12 +0900

    editアクション、updateアクション及び権限チェックの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :cheack_authority, only: [:edit, :update] 
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -22,10 +23,27 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+       redirect_to item_path(params[:id])
+    else
+      render :edit
+    end
+  end
 
  private
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :delivery_date_id, :price).merge(user_id: current_user.id)
   end
 
+  def cheack_authority
+    unless Item.find(params[:id]).user_id == current_user.id
+      redirect_to root_path
+    end
+  end     
 end
