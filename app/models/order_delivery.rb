75ee52c7d91caa84f7af62701668bad079c44e0f
katsu-skillup <seraphyk@gmail.com>
2021-08-01 02:02:58 +0900

    バリデーション修正

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -1,19 +1,17 @@
-class OederDelivery
-  include ActiveModel::model
+class OrderDelivery
+  include ActiveModel::Model
   attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id
 
   with_options presence: true do
     validates :item_id
     validates :user_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows(e.g. 123-4567" }
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows(e.g. 123-4567)" }
+    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
     validates :city
     validates :address
-    validates :phone_number, format: {with: /\A[0-9]\d{10,11}\z/, message: "Phone number is invalid. Input only number"}
-    validates :order_id
+    validates :phone_number, length: { in: 10..11, message: "is too short" } ,format: {with: /\A[0-9]\d{10,11}\z/, message: " is invalid. Input only number" }
   end
-  
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
-
+ 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
     Delivery.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
