2cbc897fd4d329e21fa4fffac21d11737c547b02
katsu-skillup <seraphyk@gmail.com>
2021-08-01 02:03:24 +0900

    createアクション設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,24 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order_delivery = OrderDelivery.new
   end
 
+  def create
+    @item = Item.find(params[:item_id])
+    @order_delivery = OrderDelivery.new(delivery_params)
+    if @order_delivery.valid?
+      @order_delivery.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+  private
+
+  def delivery_params
+    params.require(:order_delivery).permit(:postal_code, :prefecture_id, :city, :address,:building, :phone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
