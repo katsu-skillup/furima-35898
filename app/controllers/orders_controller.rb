d7990e1aedcf89844a89db11a1a84ffdb260a4a4
katsu-skillup <seraphyk@gmail.com>
2021-08-01 16:34:29 +0900

    決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,17 +9,27 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new(delivery_params)
     if @order_delivery.valid?
+      pay_item
       @order_delivery.save
       redirect_to root_path
     else
       render :index
     end
   end
-
-
+  
+  
   private
-
+  
   def delivery_params
-    params.require(:order_delivery).permit(:postal_code, :prefecture_id, :city, :address,:building, :phone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_delivery).permit(:postal_code, :prefecture_id, :city, :address,:building, :phone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: delivery_params[:token],
+      currency: 'jpy'
+    )
+  end    
 end
--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -1,8 +1,9 @@
 class OrderDelivery
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id, :token
 
   with_options presence: true do
+    validates :token
     validates :item_id
     validates :user_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows(e.g. 123-4567)" }
